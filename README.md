93cefaaaf30929efe4fd13149f4897492ccb8f3f
Cupid Valentine <westerngod@126.com>
2022-10-30 11:19:47 +0800

    finished

---
+++ README.md
@@ -0,0 +1,6 @@
+```sh
+
+cargo run -- first second third
+
+
+```
--- src/args.rs
+++ src/args.rs
@@ -9,17 +9,17 @@ fn get_nth_arg(n: usize) -> String {
 
 #[derive(Debug)]
 pub struct Args {
-    pub image_1: get_nth_arg(1),
-    pub image_2: get_nth_arg(2),
-    pub output: get_nth_arg(3),
+    pub image_1: String,
+    pub image_2: String,
+    pub output: String,
 }
 
 impl Args {
     pub fn new() -> Self {
         Args {
-            image_1: String::new(),
-            image_2: String::new(),
-            output: String::new(),
+            image_1: get_nth_arg(1),
+            image_2: get_nth_arg(2),
+            output: get_nth_arg(3),
         }
     }
 }
